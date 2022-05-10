82a496f526e5d481c2ec0e8ecce22087f632bc9e
Sergei Rakov <sergei@snoonu.com>
2022-05-10 17:45:44 +0300

    Add mappers

--- app/src/main/java/com/example/cleanarchexample/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/cleanarchexample/data/repository/UserRepositoryImpl.kt
@@ -12,19 +12,20 @@ import com.example.cleanarchexample.domain.repository.UserRepository
 class UserRepositoryImpl(private val userStorage: UserStorage): UserRepository {
 
     override fun saveName(saveParam: SaveUserNameParam): Boolean {
-        val user = User(
-            firstName = saveParam.name,
-            lastName = ""
-        )
+        val user = mapToStorage(saveParam)
         return userStorage.save(user)
     }
 
     override fun getName(): UserName {
         val user = userStorage.get()
-        val userName = UserName(
-            firstName = user.firstName,
-            lastName = user.lastName
-        )
-        return userName
+        return mapToDomain(user)
+    }
+
+    private fun UserRepositoryImpl.mapToStorage(saveParam: SaveUserNameParam): User {
+        return User(firstName = saveParam.name, lastName = "")
+    }
+
+    private fun UserRepositoryImpl.mapToDomain(user: User): UserName {
+        return UserName(firstName = user.firstName, lastName = user.lastName)
     }
 }
